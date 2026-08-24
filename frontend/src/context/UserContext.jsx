31afc40a2da77e67b9a3ab988556410ace1f11e9
Shashank Upadhyay <upshashank450@gmail.com>
2026-08-25 00:06:03 +0530

    Update server URL to production endpoint

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,14 +2,14 @@ import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
 export const userDataContext=createContext()
 function UserContext({children}) {
-    const serverUrl="http://localhost:8000"
+    const serverUrl="https://virtual-assistant-backend-4r2c.onrender.com"
     const [userData,setUserData]=useState(null)
     const [frontendImage,setFrontendImage]=useState(null)
      const [backendImage,setBackendImage]=useState(null)
      const [selectedImage,setSelectedImage]=useState(null)
     const handleCurrentUser=async ()=>{
         try {
-            const result=await axios.get(`${https://virtual-assistant-backend-4r2c.onrender.com}/api/user/current`,{withCredentials:true})
+            const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
             setUserData(result.data)
             console.log(result.data)
         } catch (error) {
